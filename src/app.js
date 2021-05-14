3c71d76c8863b08f0e0a5f7592dbe4ff3e993e60
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-05-13 23:51:15 -0300

    Change the style and consume API data
    Starts using the API to fill the form fields

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 let exchangeInfo;
 
-// fetch("https://api.ratesapi.io/api/latest").then((response)=>{
-//     return response.json()
-// }).then(jsonResponse=>{
-//     return exchangeInfo = jsonResponse
-// })
+fetch("https://api.ratesapi.io/api/latest")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsonResponse) => {
+    const rates = Object.keys(jsonResponse.rates);
+    createRatesOptions(rates)
+    return (exchangeInfo = jsonResponse);
+  });
 
+const createRatesOptions = (rates) => {
+  rates.forEach((rate) => {
+    const $select = document.querySelector("#rates");
+    const newOption = document.createElement("option");
+    newOption.textContent = rate;
+    newOption.value = rate;
+    $select.appendChild(newOption)
+  });
+};
 
-const createRatesOptions = (rate)=>{
-    const $select = document.querySelector('#rates')
-    const newOption = document.createElement('option')
-    newOption.textContent = rate
-    newOption.value = rate
-}
-
+const handleApiData = (data) => {};
