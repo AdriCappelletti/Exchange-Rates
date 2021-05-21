cb1e14e892fcc9e97c7f33d353e9b60954b42e18
Adriano Cappelletti <73238164+AdriCappelletti@users.noreply.github.com>
2021-05-21 00:00:04 -0300

    Develop (#2)
    * Change the style and consume API data
    
    Starts using the API to fill the form fields
    
    * Changes on the style
    
    * Handles the api info depending on user selection
    
    Improves the code to take the selected option by the user
    and return the specific rate for that option.
    There's also some minor changes on the style.
    
    * Finishes the first version
    
    Displays properly the selected base and its rate on a new box weh user clicks the check button.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 let exchangeInfo;
+const $checkBtn = document.querySelector("#check-btn");
 
-fetch("https://api.ratesapi.io/api/latest")
+
+fetch(
+  "http://api.exchangeratesapi.io/v1/latest?access_key=f626c631b79a9a71a8b82a6e97fe99f4"
+)
+
   .then((response) => {
     return response.json();
   })
   .then((jsonResponse) => {
     const rates = Object.keys(jsonResponse.rates);
-    createRatesOptions(rates)
+
+    createRatesOptions(rates);
     return (exchangeInfo = jsonResponse);
+  })
+  .catch((error) => {
+    console.error(error);
   });
 
 const createRatesOptions = (rates) => {
@@ -16,8 +25,47 @@ const createRatesOptions = (rates) => {
     const newOption = document.createElement("option");
     newOption.textContent = rate;
     newOption.value = rate;
-    $select.appendChild(newOption)
+
+    $select.appendChild(newOption);
   });
 };
 
-const handleApiData = (data) => {};
+const getSelectedRate = () => {
+  const $rates = document.querySelector("#rates");
+  const rateIndex = $rates.selectedIndex;
+  const selectedRate = $rates[rateIndex].value;
+  return selectedRate;
+};
+
+const getRateValue = (data, selectedRate) => {
+  const rates = data.rates;
+  selectedRate.toString();
+  const rateValue = rates[selectedRate].toFixed(2);
+  return rateValue;
+};
+const getSelectedBase = () => {
+  const $bases = document.querySelector("#bases-container");
+  const baseIndex = $bases.selectedIndex;
+  const selectedBase = $bases[baseIndex].value;
+  return selectedBase;
+};
+
+$checkBtn.onclick = (e) => {
+  const selectedBase = getSelectedBase();
+  const selectedRate = getSelectedRate();
+  const rateValue = getRateValue(exchangeInfo, selectedRate);
+  displayExchangeInfo(selectedBase, selectedRate, rateValue);
+  e.preventDefault();
+};
+
+const displayExchangeInfo = (selectedBase, selectedRate, rateValue) => {
+  const $exchangesContainer = document.querySelector("#exchanges-results");
+  const $base = document.querySelector("#base");
+  const $rate = document.querySelector("#rate");
+  const $rateValue = document.querySelector("#rate-value");
+  $base.textContent = selectedBase;
+  $rate.textContent = selectedRate;
+  $rateValue.textContent = rateValue;
+  $exchangesContainer.classList.remove("hide");
+};
+
